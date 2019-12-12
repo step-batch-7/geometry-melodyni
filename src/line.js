95d9e0e47ab8ba20fe79668e2dc85d553ec21bb1
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2019-12-12 23:44:02 +0530

    changed data structure & updated constructor method

--- src/line.js
+++ src/line.js
@@ -1,20 +1,23 @@
+"use strict";
 class Line {
-  constructor(point1X, point1Y, point2X, point2Y) {
-    this.point1X = point1X;
-    this.point1Y = point1Y;
-    this.point2X = point2X;
-    this.point2Y = point2Y;
+  constructor(endA, endB) {
+    this.endA = { x: endA.x, y: endA.y };
+    this.endB = { x: endB.x, y: endB.y };
   }
 
   isEqualTo(otherLine) {
-    const x1 = this.point1X == otherLine.point1X;
-    const y1 = this.point1Y == otherLine.point1Y;
-    const x2 = this.point2X == otherLine.point2X;
-    const y2 = this.point2Y == otherLine.point2Y;
-    return x1 && y1 && x2 && y2;
+    const i = otherLine instanceof Line;
+    const pointA =
+      this.endA.x == otherLine.endA.x && this.endA.y == otherLine.endA.y;
+    const pointB =
+      this.endB.x == otherLine.endB.x && this.endB.y == otherLine.endB.y;
+    return i && pointA && pointB;
   }
   toString() {
-    return `Line (${this.point1X},${this.point1Y})----------(${this.point2X},${this.point2Y})`;
+    return `Line (${this.endA.x},${this.endA.y})----------(${this.endB.x},${this.endB.y})`;
   }
 }
+
+console.log(line.toString());
+
 module.exports = Line;
--- test/testLine.js
+++ test/testLine.js
@@ -4,20 +4,20 @@ const assert = require("assert");
 
 describe("isEqualTo", () => {
   it("should give true if two lines are equals", () => {
-    const lineA = new Line(1, 1, 1, 10);
-    const lineB = new Line(1, 1, 1, 10);
+    const lineA = new Line({ x: 1, y: 1 }, { x: 1, y: 10 });
+    const lineB = new Line({ x: 1, y: 1 }, { x: 1, y: 10 });
     assert.strictEqual(lineA.isEqualTo(lineB), true);
   });
   it("should give false if two lines are not equals", () => {
-    const lineA = new Line(1, 1, 1, 10);
-    const lineB = new Line(1, 1, 10, 10);
+    const lineA = new Line({ x: 1, y: 1 }, { x: 1, y: 10 });
+    const lineB = new Line({ x: 1, y: 1 }, { x: 10, y: 10 });
     assert.strictEqual(lineA.isEqualTo(lineB), false);
   });
 });
 
 describe("toString", () => {
   it("should stringify the Line Object", () => {
-    const line = new Line(5, 10, 10, 10);
+    const line = new Line({ x: 5, y: 10 }, { x: 10, y: 10 });
     assert.strictEqual(line.toString(), "Line (5,10)----------(10,10)");
   });
 });
