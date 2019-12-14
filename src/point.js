246ba2d5ee04be25688a0469052d0fa978caf2b5
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2019-12-14 11:53:54 +0530

    added isEqualTo method of class point and its tests

--- src/point.js
+++ src/point.js
@@ -6,6 +6,11 @@ class Point {
     this.y = ordinate;
   }
 
+  isEqualTo(other) {
+    if (!(other instanceof Point)) return false;
+    return this.x == other.x && this.y == other.y;
+  }
+
   toString() {
     return `[Point @(${this.x},${this.y})]`;
   }
--- test/testPoint.js
+++ test/testPoint.js
@@ -11,4 +11,26 @@ describe("point", () => {
       assert.strictEqual(point.toString(), "[Point @(2,3)]");
     });
   });
+  describe("isEqualTo", () => {
+    it("should give true if two points are equal", () => {
+      const pointA = new Point(2, 3);
+      const pointB = new Point(2, 3);
+      assert.isTrue(pointA.isEqualTo(pointB));
+    });
+    it("should give false if two points are not equal", () => {
+      const pointA = new Point(2, 3);
+      const pointB = new Point(8, 10);
+      assert.isNotTrue(pointA.isEqualTo(pointB));
+    });
+    it("should give false if point is compared with other type having x, y fields", () => {
+      const pointA = new Point(2, 3);
+      const pointB = { x: 2, y: 3 };
+      assert.isNotTrue(pointA.isEqualTo(pointB));
+    });
+    it("should give false if point is compared with other type ", () => {
+      const pointA = new Point(2, 3);
+      const pointB = [];
+      assert.isNotTrue(pointA.isEqualTo(pointB));
+    });
+  });
 });
