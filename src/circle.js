fe962161c85fda82f586094d303f64ecefba1ec1
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2019-12-16 14:51:34 +0530

    added isEqualTo method in class circle

--- src/circle.js
+++ src/circle.js
@@ -1,11 +1,17 @@
 "use strict";
+const Point = require("../src/point");
 
 class Circle {
   constructor(centre, radius) {
-    this.centre = { x: centre.x, y: centre.y };
+    this.centre = new Point(centre.x, centre.y);
     this.radius = radius;
   }
 
+  isEqualTo(other) {
+    if (!(other instanceof Circle)) return false;
+    const isCommonCentre = this.centre.isEqualTo(other.centre);
+    return isCommonCentre && this.radius === other.radius;
+  }
   toString() {
     return `[Circle @(${this.centre.x},${this.centre.y}) radius ${this.radius}]`;
   }
--- test/testCircle.js
+++ test/testCircle.js
@@ -11,4 +11,16 @@ describe("circle", () => {
       assert.strictEqual(circle.toString(), expected);
     });
   });
+  describe("isEqualTo", () => {
+    it("should give true if location and size of circles are equal", () => {
+      const circleA = new Circle({ x: 0, y: 0 }, 5);
+      const circleB = new Circle({ x: 0, y: 0 }, 5);
+      assert.isTrue(circleA.isEqualTo(circleB));
+    });
+    it("should give false if location is different but size of circles are equal", () => {
+      const circleA = new Circle({ x: 0, y: 0 }, 5);
+      const circleB = new Circle({ x: 0, y: 1 }, 5);
+      assert.isFalse(circleA.isEqualTo(circleB));
+    });
+  });
 });
