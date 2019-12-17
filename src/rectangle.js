e7c7c289004e7aed6548d30427144aa95cfb5a49
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2019-12-17 18:52:04 +0530

    refactored code
    * removed endB endC private feilds
    * added getter getDaigonalPair

--- src/rectangle.js
+++ src/rectangle.js
@@ -4,33 +4,35 @@ const Point = require("./point");
 const Line = require("./line");
 
 class Rectangle {
-  #pointB;
-  #pointD;
-  constructor(endA, endB) {
-    this.pointA = new Point(endA.x, endA.y);
-    this.pointC = new Point(endB.x, endB.y);
-    this.#pointB = new Point(endB.x, endA.y);
-    this.#pointD = new Point(endA.x, endB.y);
+  constructor(endA, endC) {
+    this.endA = new Point(endA.x, endA.y);
+    this.endC = new Point(endC.x, endC.y);
   }
   toString() {
-    return `[Rectangle (${this.pointA.x},${this.pointA.y}) to (${this.pointC.x},${this.pointC.y})]`;
+    return `[Rectangle (${this.endA.x},${this.endA.y}) to (${this.endC.x},${this.endC.y})]`;
   }
   hasPoint(other) {
     if (!(other instanceof Point)) return false;
-    const AB = new Line(this.pointA, this.#pointB);
-    const BC = new Line(this.#pointB, this.pointC);
-    const CD = new Line(this.pointC, this.#pointD);
-    const AD = new Line(this.pointA, this.#pointD);
+    const { endB, endD } = this.getDiagonalPair;
+    const AB = new Line(this.endA, endB);
+    const BC = new Line(endB, this.endC);
+    const CD = new Line(this.endC, endD);
+    const AD = new Line(this.endA, endD);
     return [AB, BC, CD, AD].some(line => other.isOn(line));
-    // return other.isOn(AB) || other.isOn(BC) || other.isOn(CD) || other.isOn(AD);
+  }
+
+  get getDiagonalPair() {
+    const endB = new Point(this.endC.x, this.endA.y);
+    const endD = new Point(this.endA.x, this.endC.y);
+    return { endB, endD };
   }
 
   get length() {
-    return this.pointA.findDistanceTo(this.#pointB);
+    return this.endA.findDistanceTo(this.getDiagonalPair.endB);
   }
 
   get width() {
-    return this.pointA.findDistanceTo(this.#pointD);
+    return this.endA.findDistanceTo(this.getDiagonalPair.endD);
   }
 
   get area() {
